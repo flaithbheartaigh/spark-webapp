59b781955dac78cbd04412ab84ad4991b15e6773
Alan Flaherty <flaithbheartaigh@gmail.com>
2016-02-02 15:19:32 +0000

    Updated Books list, still requires image to go with system

--- src/main/resources/data/insert-database.sql
+++ src/main/resources/data/insert-database.sql
@@ -1,5 +1,18 @@
+Delete * from books;
+
+INSERT INTO books(title, author) VALUES ('H2 database', 'WROX');
+INSERT INTO books(title, author) VALUES ('Angel of Storms (Millennium''s Rule)', 'Trudi Canavan');
+INSERT INTO books(title, author) VALUES ('Thief''s Magic (Millennium''s Rule)', 'Trudi Canavan');
+INSERT INTO books(title, author) VALUES ('The Magicians'' Guild', 'Trudi Canavan');
+INSERT INTO books(title, author) VALUES ('The Novice', 'Trudi Canavan');
+INSERT INTO books(title, author) VALUES ('The High Lord', 'Trudi Canavan');
+INSERT INTO books(title, author) VALUES ('Programming APIs With The Spark Web Framework', 'Travis Spencer and Per Wendel');
+INSERT INTO books(title, author) VALUES ('AngularJS, JavaScript, and jQuery All in One, Sams Teach Yourself', 'Brad Dayley and Brendan Dayley');
+
+/* Original inserts
 INSERT INTO books(title, author) VALUES ('jQuery for Dummies', 'dummies');
 INSERT INTO books(title, author) VALUES ('Spark Framework for Dummies', 'dummies');
 INSERT INTO books(title, author) VALUES ('Java 1.8', 'WROX');
 INSERT INTO books(title, author) VALUES ('H2 database', 'WROX');
 INSERT INTO books(title, author) VALUES ('Windows 7 in 20 Days', 'Sams');
+*/
